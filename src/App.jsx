516df54c274e892ea141551ab0a0777b366d922e
MaryamAli21e <maryamali2188@gmail.com>
2024-08-18 05:02:13 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-// src/App.jsx
+import { useState, useEffect } from "react";
+import * as starshipService from "./services/starshipService";
+import StarshipList from "./components/StarshipList";
+// import StarshipSearch from "./components/StarshipSearch";
 
 const App = () => {
+  const [Starship, setStarship] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await starshipService.show();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setStarship(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Star Wars API</h1>
+      <h2>Search</h2>
+      <div>
+        <label htmlFor="">Search Term: </label>
+        <input type="text" name="" id="" />
+        <button>Search</button>
+      </div>
+      <h2>Starships</h2>
+      <p>Number of results: {Starship.length}</p>
+      <StarshipList Starship={Starship} />
+    </>
   );
-}
+};
 
-export default App
+export default App;
---
+++ src/components/StarshipList.jsx
@@ -0,0 +1,21 @@
+const StarshipList = (props) => {
+    const starships = props.Starship.map((starship) => {
+      return (
+        <>
+          <li key={starship.name}>
+            <p>{starship.name}</p>
+            <p>Class: {starship.starship_class}</p>
+            <p>Manufacturer: {starship.manufacturer}</p>
+            <p>Model: {starship.model}</p>
+          </li>
+        </>
+      );
+    });
+    return (
+      <section>
+        <ul>{starships}</ul>
+      </section>
+    );
+  };
+  
+  export default StarshipList;
